28e6d1fce6910d785fbe986de5477b5e926bac72
Stanton Wong <stanton.wong@gmail.com>
2016-03-19 16:06:58 +0800

    added basic UI structure for Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,24 @@ import styles from './Header.css';
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <div>Header content</div>
+      <div data-ui-component='Header__container' className={styles.container}>
+        {/* Logo */}
+        <div data-ui-component='Header__logo' className={styles.logo}>
+          Mario's Butterfly
+        </div>
+
+        {/* Project Name and Selector */}
+        <div data-ui-component='Header__projectNameWrapper' className={styles.projectNameWrapper}>
+          <div data-ui-component='Header__projectName' className={styles.projectName}>
+            Project: Mario's Butterfly
+          </div>
+        </div>
+
+        {/* User Info */}
+        <div data-ui-component='Header__userInfo' className={styles.userInfo}>
+          Stanton Wong
+        </div>
+
       </div>
     );
   }
